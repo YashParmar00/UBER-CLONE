220da101b9badca3b363320bd885210e142fcfbe
YashParmar00 <yashsingh0033@gmail.com>
2025-05-08 00:49:52 +0530

    package updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['uber-clone-1-4vn5.onrender.com', 'localhost'], // Add your Render host here
+  },
 })
